1f321f7787975b6419bd7600eab48d7a31725f8d
Patrick Burleson <pburleson@gmail.com>
2012-01-18 12:25:53 -0600

    Added sample code

--- UndoTest/BBQMainViewController.m
+++ UndoTest/BBQMainViewController.m
@@ -8,7 +8,17 @@
 
 #import "BBQMainViewController.h"
 
+#import <objc/runtime.h>
+
+#import "BBQAppDelegate.h"
+#import "BBQBar.h"
+#import "BBQFoo.h"
+
 @interface BBQMainViewController ()
+
+@property (nonatomic, retain) BBQFoo *foo;
+
+- (void)handlePanGestuer:(UIPanGestureRecognizer *)gesture;
 
 @end
 
@@ -16,25 +26,154 @@
 
 @synthesize dragView = _dragView;
 @synthesize managedObjectContext = _managedObjectContext;
+@synthesize foo = _foo;
+
+#pragma mark - Memory Management
+
+- (void)dealloc
+{
+    [_managedObjectContext release];
+    [_dragView release];
+    [_foo release];
+    [super dealloc];
+}
+
+#pragma mark - View Lifecycle
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:[BBQFoo entityName]];
+    NSError *error = nil;
+    NSArray *foos = [[UIApp managedObjectContext] executeFetchRequest:request error:&error];
+    if ( !foos )
+    {
+        NSLog(@"Error executing fetch for Foo: %@", error);
+        abort();
+    }
+    
+    if ( [foos count] == 0 )
+    {
+        [self setFoo:[BBQFoo insertInManagedObjectContext:[UIApp managedObjectContext]]];
+        [[self foo] setTestTime:[NSDate date]];
+        [UIApp saveContext];
+    }
+    else
+    {
+        [self setFoo:[foos objectAtIndex:0]];
+    }
+    
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGestuer:)];
+    [panGesture setMinimumNumberOfTouches:1];
+    [panGesture setMaximumNumberOfTouches:1];
+    [[self dragView] addGestureRecognizer:panGesture];
+    [panGesture release];
 }
 
 - (void)viewDidUnload
 {
     [self setDragView:nil];
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
 }
 
-- (void)dealloc
+#pragma mark - Pan Gesture Handler
+
+- (void)handlePanGestuer:(UIPanGestureRecognizer *)gesture
 {
-    [_managedObjectContext release];
-    [_dragView release];
-    [super dealloc];
+    NSUndoManager *undoManager = [[UIApp managedObjectContext] undoManager];
+    NSMutableString *stateString = [NSMutableString stringWithString:@"Gesture State: "];
+    switch (gesture.state) {
+        case UIGestureRecognizerStateBegan:
+            [stateString appendString:@"Began"];
+            break;
+        case UIGestureRecognizerStateEnded:
+            [stateString appendString:@"Ended"];
+            break;
+        case UIGestureRecognizerStateCancelled:
+            [stateString appendString:@"Cancelled"];
+            break;
+        case UIGestureRecognizerStateFailed:
+            [stateString appendString:@"Failed"];
+            break;
+        case UIGestureRecognizerStateChanged:
+            [stateString appendString:@"Changed"];
+            break;
+        case UIGestureRecognizerStatePossible:
+            [stateString appendString:@"Possible"];
+            break;
+        default:
+            break;
+    }
+    
+    NSLog(@"%@", stateString);
+    
+    if (gesture.state == UIGestureRecognizerStateBegan)
+    {
+        [[UIApp managedObjectContext] processPendingChanges];
+        id undoStack = nil;
+        
+        object_getInstanceVariable(undoManager, "_undoStack", &undoStack);
+        NSLog(@"Undo stack at begin: %@", [undoStack description]);
+        
+        NSInteger undoGroupCount = [undoManager groupingLevel];
+        NSLog(@"Undo grouping level count at begin: %i", undoGroupCount);
+        
+        if (undoGroupCount > 0)
+        {
+            [undoManager endUndoGrouping];
+            undoGroupCount = [undoManager groupingLevel];
+            NSLog(@"Undo grouping level count after closing existing group: %i", undoGroupCount);
+        }
+        
+        
+        [undoManager setGroupsByEvent:NO];
+        [undoManager beginUndoGrouping];
+        undoGroupCount = [undoManager groupingLevel];
+        NSLog(@"Undo grouping level count after adding grouping: %i", undoGroupCount);
+        
+        undoStack = nil;
+        object_getInstanceVariable(undoManager, "_undoStack", &undoStack);
+        NSLog(@"Undo stack at after begin undo group: %@", [undoStack description]);
+    }
+    else if (gesture.state == UIGestureRecognizerStateEnded)
+    {
+        NSInteger undoGroupCount = [undoManager groupingLevel];
+        NSLog(@"Undo grouping level count at end: %i", undoGroupCount);
+        
+        [[UIApp managedObjectContext] processPendingChanges];
+        [undoManager endUndoGrouping];
+        [undoManager setGroupsByEvent:YES];
+        undoGroupCount = [undoManager groupingLevel];
+        NSLog(@"Undo grouping level count after removing grouping and adding one: %i", undoGroupCount);
+        
+        id undoStack = nil;
+        object_getInstanceVariable(undoManager, "_undoStack", &undoStack);
+        NSLog(@"Undo stack at end: %@", [undoStack description]);
+    }
+    else if (gesture.state == UIGestureRecognizerStateCancelled)
+    {
+        NSInteger undoGroupCount = [undoManager groupingLevel];
+        NSLog(@"Undo grouping level count at cancel: %i", undoGroupCount);
+        [undoManager endUndoGrouping];
+        [undoManager setGroupsByEvent:YES];
+        [undoManager beginUndoGrouping];
+        undoGroupCount = [undoManager groupingLevel];
+        NSLog(@"Undo grouping level count after removing grouping and adding one: %i", undoGroupCount);
+        [undoManager undo];
+    }
+    else
+    {
+        [[self foo] setTestTime:[NSDate date]];
+        NSInteger undoGroupCount = [undoManager groupingLevel];
+        NSLog(@"Undo grouping level count at change: %i", undoGroupCount);
+        
+        id undoStack = nil;
+        object_getInstanceVariable(undoManager, "_undoStack", &undoStack);
+        NSLog(@"Undo stack at change: %@", [undoStack description]);
+        
+        
+    }
 }
 
 @end
